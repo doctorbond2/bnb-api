2a8d9a3ce66c2041c6b679be4e0e0695e9a2f316
doctorbond2 <michael.rostlund@hotmail.com>
2024-10-07 20:50:48 +0200

    Refactor database models and API routes
    - Removed unused Todo model and API routes
    - Added Property model and API routes for creating new properties
    - Updated User model to include bookings relationship
    - Updated Property model to include bookings relationship
    - Added helper function for validating properties

--- src/app/api/todos/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse, NextRequest } from 'next/server';
-import { CreateTodo } from '@/utils/handlers/Todo/post';
-export async function GET() {
-  return NextResponse.json({ status: 200 });
-}
-export async function POST(req: NextRequest): Promise<Response> {
-  return await CreateTodo(req);
-}
---
+++ src/utils/handlers/Property/post.ts
@@ -0,0 +1,5 @@
+import { NextRequest, NextResponse } from 'next/server';
+import ResponseError from '@/models/classes/responseError';
+export async function handler_createNewProperty(
+  req: NextRequest
+): Promise<Response> {}
