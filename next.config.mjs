8a4cb55b983b415b833f3554b3f9685322e76f09
doctorbond2 <michael.rostlund@hotmail.com>
2024-11-04 11:55:54 +0100

    Add additional allowed origin for CORS in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,10 @@ const nextConfig = {
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           {
             key: 'Access-Control-Allow-Origin',
-            value: 'http://localhost:8080',
+            value: [
+              'http://localhost:8080',
+              'https://bnb-frontend-delta.vercel.app',
+            ],
           },
           { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE' },
           {
