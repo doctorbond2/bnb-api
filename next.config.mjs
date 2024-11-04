37701abf804e111f62b3c17c6eaa0e487d537a2e
doctorbond2 <michael.rostlund@hotmail.com>
2024-11-04 12:02:39 +0100

    Update CORS configuration to use environment variable for allowed origin

--- next.config.mjs
+++ next.config.mjs
@@ -13,10 +13,7 @@ const nextConfig = {
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           {
             key: 'Access-Control-Allow-Origin',
-            value: [
-              'http://localhost:8080',
-              'https://bnb-frontend-delta.vercel.app',
-            ],
+            value: process.env.HEADER_URL,
           },
           { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE' },
           {
