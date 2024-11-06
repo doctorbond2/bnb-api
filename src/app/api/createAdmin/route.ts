67abb5b4472c2d01f43508eb4a29bfe91697394c
doctorbond2 <michael.rostlund@hotmail.com>
2024-11-06 16:41:40 +0100

    Remove debug logging from admin creation route and streamline password validation

--- src/app/api/createAdmin/route.ts
+++ src/app/api/createAdmin/route.ts
@@ -4,13 +4,9 @@ const adminPassword = process.env.ADMIN_CREATE_PW;
 import { hashPassword } from '@/utils/helpers/password';
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  console.log('adminPassword:', adminPassword);
-  console.log('body.admin_password:', body.admin_password);
   if (body.admin_password !== adminPassword) {
-    console.log('Unauthorized');
     return NextResponse.json({ status: 401 });
   }
-  console.log('req:', body);
   const hashedPassword = await hashPassword(body.password);
   const newAdmin = await db.admin.createAdmin(body, hashedPassword);
   console.log(newAdmin.deletedAt);
