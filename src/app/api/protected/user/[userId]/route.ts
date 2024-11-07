4fd705a04887538c916c9be5d05a30992a0fa7a9
doctorbond2 <michael.rostlund@hotmail.com>
2024-11-07 16:36:01 +0100

    Add user status retrieval and update route parameter naming

--- src/app/api/protected/user/[userId]/route.ts
+++ src/app/api/protected/user/[userId]/route.ts
@@ -1,14 +1,22 @@
 import { NextResponse, NextRequest } from 'next/server';
 import { handler_DeleteUser } from '@/utils/handlers/User/delete';
+import { handler_GetUserStatus } from '@/utils/handlers/User/get';
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: { userId: string } }
 ): Promise<Response> {
-  return NextResponse.json({ message: 'Returned user: ' + params.id });
+  const { searchParams } = new URL(req.url);
+  const status = searchParams.get('status');
+  if (!status) {
+    return NextResponse.json({ message: 'Returned user: ' + params.userId });
+  }
+  console.log('status: ', status);
+  console.log('id: ', params.userId);
+  return await handler_GetUserStatus(params.userId);
 }
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: { userId: string } }
 ): Promise<Response> {
-  return await handler_DeleteUser(req, params.id);
+  return await handler_DeleteUser(req, params.userId);
 }
--- src/utils/handlers/User/get.ts
+++ src/utils/handlers/User/get.ts
@@ -37,6 +37,35 @@ export async function handler_GetUserById(id: string): Promise<Response> {
   }
   return NextResponse.json(user, { status: 200 });
 }
+export const handler_GetUserStatus = async (id: string) => {
+  if (!id) {
+    return NextResponse.json(
+      { message: 'User ID is required' },
+      { status: 400 }
+    );
+  }
+  const userStatus = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      hosted_properties: {
+        include: { _count: true },
+        where: { deletedAt: null },
+      },
+      bookings: {
+        include: { _count: true },
+        where: { NOT: { status: { in: ['cancelled', 'rejected'] } } },
+      },
+    },
+  });
+  console.log(userStatus);
+  if (!userStatus) {
+    return NextResponse.json({ message: 'User not found' }, { status: 404 });
+  }
+  return NextResponse.json({ data: userStatus, status: 200 });
+};
 // export async function getUserById(id: number) {
 //   const user = await prisma.user.findUnique({
 //     where: {
